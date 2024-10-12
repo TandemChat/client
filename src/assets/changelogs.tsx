4841931372b8867214e33469365cc8a88d1df1b4
Gru <78332542+gru2007@users.noreply.github.com>
2024-10-12 15:20:55 +1000

    Changelog1

--- src/assets/changelogs.tsx
+++ src/assets/changelogs.tsx
@@ -20,23 +20,18 @@ export interface ChangelogPost {
 }
 
 export const changelogEntries: Record<number, ChangelogPost> = {
-    // 1: {
-    //     date: new Date("2022-06-12T20:39:16.674Z"),
-    //     title: "Title",
-    //     content: [
-    //         "Paragraph.",
-    //         {
-    //             type: "image",
-    //             src: "https://autumn.revolt.chat/attachments/E21kwmuJGcASgkVLiSIW0wV3ggcaOWjW0TQF7cdFNY/image.png",
-    //         },
-    //         "Paragraph.",
-    //         {
-    //             type: "image",
-    //             src: "https://autumn.revolt.chat/attachments/LWRYoKR2tE1ggW_Lzm547P1pnrkNgmBaoCAfWvHE74/image.png",
-    //         },
-    //         "Paragraph!",
-    //     ],
-    // }
+    1: {
+        date: new Date("2024-10-11T20:39:16.674Z"),
+        title: "@everyone, мы добавили пинг @everyone!",
+        content: [
+            "Мы добавили возможность отправлять пинг @everyone! Так что, теперь вы можете держать всех на своём сервере в курсе новостей.",
+            // {
+            //     type: "image",
+            //     src: "https://autumn.revolt.chat/attachments/E21kwmuJGcASgkVLiSIW0wV3ggcaOWjW0TQF7cdFNY/image.png",
+            // },
+            "В будущем мы добавим отдельное право на использование @everyone, пока что доступ есть у всех, у кого есть право на упоминания. Упоминания ролей так же будут позже.",
+        ],
+    }
 };
 
 export const changelogEntryArray = Object.keys(changelogEntries).map(
