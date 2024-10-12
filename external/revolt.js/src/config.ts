e03c732fbd09fb48f132770352e656afb7cef8a7
Gru <ruslru376@gmail.com>
2024-10-12 10:09:10 +1000

    our REVOLT

---
+++ external/revolt.js/src/config.ts
@@ -0,0 +1,13 @@
+export const LIBRARY_VERSION = "6.0.17";
+
+export const defaultConfig = {
+    apiURL: "https://app.tandemchat.ru/api",
+    autoReconnect: true,
+    heartbeat: 30,
+    debug: false,
+
+    cache: false,
+    unreads: false,
+
+    ackRateLimiter: true,
+};
